593db97f277af666ca956319f9f085ca708fa48b
MikeeBuilds <citizensofandrometta@gmail.com>
2024-09-09 14:13:54 -0400

    taking a break

---
+++ .history/src/app/(landing)/_components/navbar/menu_20240909141021.tsx
@@ -0,0 +1,26 @@
+"use client"
+
+import { Card, CardContent } from "@/components/ui/card"
+
+type MenuProps = {
+    orientation: "moblile" | "desktop"
+}
+
+type Props = { orientation: MenuProps }
+
+const Menu = ({ orientation }: MenuProps) => {
+    const { section, onSetSection } = useNavigation()
+    switch (orientation) {
+        case "desktop":
+            return (
+                <Card className="bg-themeGray border-themeGray bg-clip-padding backdrop--blur__safari backdrop-filter backdrop-blur-2xl bg-opacity-60 p-1 lg:flex hidden rounded-xl">
+                    <CardContent className="p-0 flex gap-2"></CardContent>
+                </Card>
+            )
+        case "moblile":
+            return <div></div>
+            desfault: return <></>
+    }
+}
+
+export default Menu
